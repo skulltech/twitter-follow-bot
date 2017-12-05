b700419d1ee1ac8646fbb0550baf1a15c08f6362
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-05 21:09:07 +0530

    Modified the script a bit

--- twitter-bot.py
+++ twitter-bot.py
@@ -4,7 +4,9 @@ import time
 from configparser import ConfigParser
 
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def read_usernames(filename):
@@ -16,10 +18,9 @@ def read_usernames(filename):
     return usernames
 
 
-def twitter_login(driver, settings):
+def login(driver, settings):
     driver.get('https://twitter.com/')
-    login_button = driver.find_element_by_class_name('StreamsLogin')
-    login_button.click()
+    driver.find_element_by_class_name('StreamsLogin').click()
 
     username = driver.find_element_by_name('session[username_or_email]')
     username.send_keys(settings['Username'])
@@ -29,37 +30,36 @@ def twitter_login(driver, settings):
     username.submit()
 
 
-def follow_user(driver, username):
-    url = 'https://twitter.com/' + username
-    driver.get(url)
-    follow_button = driver.find_element_by_class_name('follow-button')
+def follow(driver, username):
+    driver.get('https://twitter.com/' + username)
+    wait = WebDriverWait(driver, 10)
+    follow = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'follow-button')))
+    
+    if 'Following' in follow.text:
+        return
+    elif 'Follow' in follow.text:
+        follow.click()
+
+def main():
+    driver = webdriver.Chrome()
+
     while True:
-        if follow_button.text == 'Following':
-            return
-        elif follow_button.text == 'Follow':
-            follow_button.click()
+        filename = input('[?] The filename of the csv: ')
+        try:
+            usernames = read_usernames(filename)
+        except FileNotFoundError:
+            print('[!] File not found! Try again...')
+            continue
+        else:
+            break
 
-driver = webdriver.Chrome()
+    urls = ['https://twitter.com/' + username for username in usernames]
+    config_file = ConfigParser()
+    config_file.read('config.ini')
+    settings = config_file['SETTINGS']
 
-while True:
-    filename = input('Enter the filename of the csv list [with the .csv extension]: ')
-    try:
-        usernames = read_usernames(filename)
-    except FileNotFoundError:
-        print('File not found! Try again...')
-        continue
-    else:
-        break
-
-urls = ['https://twitter.com/' + username for username in usernames]
-config_file = ConfigParser()
-config_file.read('config.ini')
-
-settings = config_file['SETTINGS']
-
-twitter_login(driver, settings)
-
-for username in usernames:
-    print('Following user: {username}'.format(username=username))
-    follow_user(driver, username)
-    time.sleep(random.randint(int(settings['Mintime']), int(settings['Maxtime'])))
+    login(driver, settings)
+    for username in usernames:
+        print('Following user: {username}'.format(username=username))
+        follow(driver, username)
+        time.sleep(random.randint(int(settings['Mintime']), int(settings['Maxtime'])))
